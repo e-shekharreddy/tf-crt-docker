6f225701cc466ae167fc9f9f8e490ce387ef8418
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-04-01 11:50:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "docker" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
-  user_data = file("bastion.sh")
+  user_data = file("docker.sh")
 
   tags = {
     Name = "docker"
