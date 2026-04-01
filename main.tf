1ec5492c3e06215e57507f0486d5c7732bf5f2de
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-04-01 13:00:30 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ resource "aws_instance" "docker" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
   user_data = file("docker.sh")
-
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
   tags = {
     Name = "docker"
     Project = "docker-roboshop"
