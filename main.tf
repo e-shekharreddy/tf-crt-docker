08653da2060b15711b1b2b7fe899641255ae2748
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-04-06 12:53:30 +0530

    k8

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "docker" {
+resource "aws_instance" "workstation" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
